7880c8f5877340e510a4a6589c6f5df784ea1350
adamg2609@gmail.com <ghebrenegusab@cardiff.ac.uk>
2024-02-08 19:55:10 +0000

    Basic Calendar Functionality

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+function EventsCalendar(){
+    const localizer = momentLocalizer(moment);
+
+    const trainerEvents = [
+        {
+            start: new Date(2024, 1, 8, 12, 30),
+            end: new Date(2024, 1, 8, 14, 30),
+            title: "Meeting with Gavin from Acute Training Solutions - Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium",
+            location: "Bute Street"
+        }
+    ];
+
+    return (
+        <div>
+            <h3>Calendar</h3>
+            <Calendar
+            localizer={localizer}
+            events={trainerEvents}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }}></Calendar>
+        </div>
+    );
+}
+
+export default EventsCalendar;
---
+++ client/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Heading(){
+    return(
+        <h1>Acute Training Solutions LTD</h1>
+    );
+}
+
+export default Heading;
